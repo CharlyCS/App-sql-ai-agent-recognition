2730b5b5f77c058f6cd875b4abb75f0efe9d47bc
CharlyCS <ccoronels@uni.pe>
2024-09-19 19:08:45 -0500

    alpha version

---
+++ speech.py
@@ -0,0 +1,18 @@
+import speech_recognition as sr
+r = sr.Recognizer()
+
+def transcription():
+    with sr.Microphone() as source:
+        print("Por favor, hable ahora...")
+        r.adjust_for_ambient_noise(source)
+        audio = r.listen(source)
+        try:
+            texto = r.recognize_google(audio, language="es-ES")
+            print(texto)
+            return texto
+        except sr.UnknownValueError:
+            print("No se pudo entender el audio")
+        except sr.RequestError as e:
+            print(f"Error al conectar: {e}")
+
+texto_transcrito = transcription()
